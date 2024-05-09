9fa73166c90d4a6b51bd41ddbb8059461b2c3098
Jeremy Vincent Yu <jtyu3@up.edu.ph>
2024-05-09 14:58:13 +0800

    Pointed Source Code Link at New Repository and Adjusted README to reflect the successful creation of easy launchers

--- rom/eboto_frontend/src/BottomBar.tsx
+++ rom/eboto_frontend/src/BottomBar.tsx
@@ -4,7 +4,7 @@ export default function BottomBar(){
     return (
         <Toolbar>
             <MenuItem>
-            <Link href="https://github.com/jeremyvincentyu/eBoto2">
+            <Link href="https://github.com/jeremyvincentyu/eboto_builder">
             <Typography>
                 Source Code
             </Typography>
