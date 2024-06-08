611892a1f851e7fb43c5566e0808d265daa0fb6a
Jeremy Vincent Yu <jtyu3@up.edu.ph>
2024-06-08 08:55:28 +0800

    Fixed Incorrect ABI Path

--- rom/performance_packaged/src/control_account.py
+++ rom/performance_packaged/src/control_account.py
@@ -37,7 +37,7 @@ class ControlVoter:
             address_object = json.loads(address_string)
             actual_address  = self.web3_instance.to_checksum_address(address_object["eBoto#EA_Account"])
         
-        with open("abi.json","r") as abi_file:
+        with open("data/abi.json","r") as abi_file:
             abi_string = abi_file.read()
             actual_abi = json.loads(abi_string)
         
